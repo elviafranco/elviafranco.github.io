2fc071b8e7d1107ec849ba82ce60fc4f6ac7dafc
elvia.franco <evfranco@hush.ai>
2021-08-05 22:47:59 -0700

    Removed .cache

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/evf/WORK/DEV/portfolio/elviafranco.github.io/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/evf/WORK/DEV/portfolio/elviafranco.github.io/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/evf/WORK/DEV/portfolio/elviafranco.github.io/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/evf/WORK/DEV/portfolio/elviafranco.github.io/src/pages/index.js"))
 }
 
