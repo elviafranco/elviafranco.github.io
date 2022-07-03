59184b1dd40ce4ae66285abb20a7204a131e0d9e
elviafranco <evf@Es-Macbook.local>
2022-07-02 18:09:27 -0600

    Revisions to template

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMode(mode) {
     mode.classList.toggle('fa-moon-stars');
     document.body.classList.toggle('dark');
 }
+
+function showContent() {
+    let section = document.getElementById('test');
+    section.style.display = 'block';
+}
+
+function showSection() {
+    let section = document.getElementsByClassName('section-phase');
+    section.show();
+}
