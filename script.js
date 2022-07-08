d99e6ef69483d1a1e42aa99fc7ac37db5f1efe37
elviafranco <evf@Es-Macbook.local>
2022-07-07 18:28:11 -0600

    Added section toggles

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
+// Dark Mode Toggle
 function toggleMode(mode) {
     mode.classList.toggle('fa-moon-stars');
     document.body.classList.toggle('dark');
 }
 
-function showContent() {
-    let section = document.getElementById('test');
-    section.style.display = 'block';
+// Buttons
+let discoverbtn = document.getElementById('discover-btn');
+let definebtn = document.getElementById('define-btn');
+let developbtn = document.getElementById('develop-btn');
+let deliverbtn = document.getElementById('deliver-btn');
+
+// Sections
+let discover = document.getElementById('discover');
+let define = document.getElementById('define');
+let develop = document.getElementById('develop');
+let deliver = document.getElementById('deliver');
+
+// Section Toggles
+
+function openDiscover() {
+    discover.classList.toggle('hidden');
+    define.classList.add('hidden');
+    develop.classList.add('hidden');
+    deliver.classList.add('hidden');
 }
 
-function showSection() {
-    let section = document.getElementsByClassName('section-phase');
-    section.show();
+function openDefine() {
+    define.classList.toggle('hidden');
+    discover.classList.add('hidden');
+    develop.classList.add('hidden');
+    deliver.classList.add('hidden');
 }
+
+function openDevelop() {
+    develop.classList.toggle('hidden');
+    discover.classList.add('hidden');
+    define.classList.add('hidden');
+    deliver.classList.add('hidden');
+}
+
+function openDeliver() {
+    deliver.classList.toggle('hidden');
+    discover.classList.add('hidden');
+    define.classList.add('hidden');
+    develop.classList.add('hidden');
+}
+
+// btn.addEventListener('click', open);
+// btn.addEventListener('click', close);
+discoverbtn.onclick = openDiscover;
+definebtn.onclick = openDefine;
+developbtn.onclick = openDevelop;
+deliverbtn.onclick = openDeliver;
