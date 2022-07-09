69307c0c9b5ca84afd079ac8a872c84d8f8ae6d3
elviafranco <evf@Es-Macbook.local>
2022-07-08 22:06:45 -0600

    Revised images and completed template

--- script.js
+++ script.js
@@ -8,16 +8,13 @@ function toggleMode(mode) {
 let discoverbtn = document.getElementById('discover-btn');
 let definebtn = document.getElementById('define-btn');
 let developbtn = document.getElementById('develop-btn');
-let deliverbtn = document.getElementById('deliver-btn');
 
 // Sections
 let discover = document.getElementById('discover');
 let define = document.getElementById('define');
 let develop = document.getElementById('develop');
-let deliver = document.getElementById('deliver');
 
 // Section Toggles
-
 function openDiscover() {
     discover.classList.toggle('hidden');
     define.classList.add('hidden');
@@ -39,16 +36,10 @@ function openDevelop() {
     deliver.classList.add('hidden');
 }
 
-function openDeliver() {
-    deliver.classList.toggle('hidden');
-    discover.classList.add('hidden');
-    define.classList.add('hidden');
-    develop.classList.add('hidden');
-}
-
-// btn.addEventListener('click', open);
-// btn.addEventListener('click', close);
 discoverbtn.onclick = openDiscover;
 definebtn.onclick = openDefine;
 developbtn.onclick = openDevelop;
-deliverbtn.onclick = openDeliver;
+
+
+const activePage = window.location;
+console.log(activePage);
