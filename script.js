41072acdb72ffd48440ff40500d63d4750f18b28
Elvia <evf@Es-Macbook.local>
2022-08-07 21:30:13 -0600

    Spotify api

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const activePage = window.location.pathname;
 
 const navLinks = document.querySelectorAll("nav li a");
@@ -9,18 +8,27 @@ navLinks.forEach((link) => {
 });
 
 // Fetch from Spotify API
-fetch("https://api.spotify.com/v1/me/top/tracks?limit=10&offset=5", {
+fetch("https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10&offset=0", {
   method: "GET",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
     Authorization:
-      "Bearer " + process.env.TOKEN
+      "Bearer BQCGD8K-K7jRECk9qpSLYpKZvLCHrGs7NI0PwZIySYIXSJgkyElDK3Tb4YKGtd0NJyFAcVFcuZMOxXzHjuU8u5_dwiT6UJydb8Z-yjqGkjuLhiidbx2BqfL4yw7QOZPaGcMUE-49xrGwaBHhpYK18gXW8nN0c4yySL0y1hBH55BNgkTJhFoqDMh3RasRJ9WkIw"
   },
 })
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    data.items.forEach(element => {
+      console.log(element.album.name);
+      const img = document.createElement('img');
+      // make an a tag and adjust href so it's clickable!
+      img.src = element.album.images[0].url;
+      img.setAttribute ('href', element.album.external_urls.spotify);
+      img.classList.add('album');
+      document.querySelector('.image-container').appendChild(img);
+    });
   })
   .catch((err) => {
     console.log(`error ${err}`);
