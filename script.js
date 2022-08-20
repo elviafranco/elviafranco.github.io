c1b627d99c73e8c8e063b5178275f49ed0694c02
Elvia <evf@Es-Macbook.local>
2022-08-20 15:54:18 -0600

    Removed old code

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ navLinks.forEach((link) => {
   }
 });
 
-if (activePage.includes('index')){
+if (activePage.includes('/')){
   document.querySelector('.portrait-container').classList.add('on');
 }
 
@@ -44,38 +44,5 @@ function toggleMode(mode) {
   document.body.classList.toggle("dark");
 }
 
-// Buttons
-let discoverbtn = document.getElementById("discover-btn");
-let definebtn = document.getElementById("define-btn");
-let developbtn = document.getElementById("develop-btn");
 
-// Sections
-let discover = document.getElementById("discover");
-let define = document.getElementById("define");
-let develop = document.getElementById("develop");
 
-// Section Toggles
-function openDiscover() {
-  discover.classList.toggle("hidden");
-  define.classList.add("hidden");
-  develop.classList.add("hidden");
-  deliver.classList.add("hidden");
-}
-
-function openDefine() {
-  define.classList.toggle("hidden");
-  discover.classList.add("hidden");
-  develop.classList.add("hidden");
-  deliver.classList.add("hidden");
-}
-
-function openDevelop() {
-  develop.classList.toggle("hidden");
-  discover.classList.add("hidden");
-  define.classList.add("hidden");
-  deliver.classList.add("hidden");
-}
-
-discoverbtn.onclick = openDiscover;
-definebtn.onclick = openDefine;
-developbtn.onclick = openDevelop;
