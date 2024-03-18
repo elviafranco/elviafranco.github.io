c6fc48dbeffdefb4129ce6d1d70b17a02bfb594e
Elvia <elvia.franco@icloud.com>
2024-03-17 23:01:14 -0700

    Adding Firebase

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFnmFVQhXuLvk1wt5CTtWWN4I04PRstHQ",
+  authDomain: "elviafranco.firebaseapp.com",
+  projectId: "elviafranco",
+  storageBucket: "elviafranco.appspot.com",
+  messagingSenderId: "1010759718664",
+  appId: "1:1010759718664:web:8937b211248aab6115bebd",
+  measurementId: "G-T9TVY2HEJE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const activePage = window.location.pathname;
 console.log(activePage);
 
