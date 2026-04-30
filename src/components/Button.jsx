d5df04abda514ed459eb85537bf02fef92c9e2f9
Elvia <elvia.franco@icloud.com>
2026-04-29 17:07:35 -0700

    Ok dark mode, i see you

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,10 @@ import { NavLink } from "react-router";
 
 function Button({ label, link }) {
   return (
-    <NavLink className="bg-fuchsia-500 text-white" to={link}>
+    <NavLink
+      className="px-4 py-2 inline-flex items-center justify-center rounded-lg text-sm font-medium border-2 border-[var(--border)] hover:text-[var(--accent)] hover:bg-[var(--accent-bg)] hover:border-[var(--accent-border)]"
+      to={link}
+    >
       {label}
     </NavLink>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,46 @@
 import { NavLink } from "react-router";
+import { useState, useEffect } from "react";
 import { Sun, Moon } from "lucide-react";
 import Avatar from "../assets/headshot.jpg";
 import Button from "./Button";
 
 function Navbar() {
+  const [theme, setTheme] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light",
+  );
+
+  function toggleTheme() {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  }
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
   return (
-    <nav>
+    <nav className="my-10 mx-4">
       <div className="flex justify-around">
         <NavLink to="/">
           <img
             src={Avatar}
             alt="headshot"
-            className="size-10 rounded-full hover:border-2 border-fuchsia-500/25"
+            className="size-11 rounded-full border-2 border-[var(--border)] hover:border-[var(--accent-border)]"
           />
         </NavLink>
         <Button link="/kite" label="Kite" />
         <Button link="/lesgo" label="Lesgo" />
         <Button link="/ribi" label="Ribi" />
-        <button>
-          <Sun />
+        <button
+          className="p-2 rounded-lg border-2 border-[var(--border)] hover:text-[var(--accent)] hover:border-[var(--accent-border)]"
+          onClick={toggleTheme}
+        >
+          {theme === "light" ? <Sun /> : <Moon />}
         </button>
       </div>
     </nav>
