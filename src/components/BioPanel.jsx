5f6f0d082defc91376f7383ad188c9c00663c090
Elvia <elvia.franco@icloud.com>
2026-05-11 16:21:46 -0700

    Reworking cards

--- src/components/BioPanel.jsx
+++ src/components/BioPanel.jsx
@@ -1,11 +1,35 @@
 import InfoCard from "./InfoCard";
+import asu from "../assets/asu.png";
 
 function BioPanel() {
   return (
-    <div className="px-4">
-      <InfoCard label="Education">
-        <p>Testing</p>
+    <div className="grid grid-flow-row-dense grid-cols-1 grid-rows-3 gap-4">
+      <InfoCard label="Overview">
+        <p>
+          Elvia Franco is a digital jack of all trades with experience in
+          design, development, marketing and strategy. These days, she's an
+          everything woman @creovia.io with a passion for creating beautiful and
+          functional web experiences.
+        </p>
       </InfoCard>
+      <div className="grid grid-cols-2 gap-4">
+        <InfoCard label="Education">
+          <div>
+            <p>Management, M.S.</p>
+            <p>Arizona State University</p>
+          </div>
+          <div
+            className="p-2 w-24 h-auto rounded-lg 
+        bg-[var(--bg-s)]"
+          >
+            <img src={asu} alt="asulogo" className="object-contain" />
+          </div>
+        </InfoCard>
+        <InfoCard label="Personality">
+          <p>INTJ</p>
+        </InfoCard>{" "}
+        <InfoCard label="Tech Stack"></InfoCard>
+      </div>
     </div>
   );
 }
--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,7 +1,7 @@
 function InfoCard({ label, children }) {
   return (
-    <div className="p-4 rounded-lg border-2 border-[var(--border)]">
-      <h3>{label}</h3>
+    <div className="flex flex-col p-4 rounded-lg overflow-hidden border-2 border-[var(--border)] hover:bg-[var(--bg-s)]">
+      <h3 className="mb-3">{label}</h3>
       {children}
     </div>
   );
